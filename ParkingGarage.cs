b765e70ccf15a5a98f1a3620866ee1bf1e538fdf
LeonardoTrein <98125531+LeonardoTrein@users.noreply.github.com>
2023-11-27 11:25:13 +0000

    Add files via upload

---
+++ ParkingGarage.cs
@@ -0,0 +1,54 @@
+﻿using System;
+namespace ca1_oop_24332.sln
+{
+    public class ParkingGarage
+    {
+        public ParkingGarage()
+        {
+
+
+        private double minimumFee = 2.0;
+        private double additionalFeeG1 = 0.5;
+        private double additionalFeeG2 = 0.6;
+        private double additionalFeeG3 = 0.75;
+        private double maxCharge = 10.00;
+        private double totalHours = 0;
+
+
+        private double garage1Total = 0;
+        private double garage2Total = 0;
+        private double garage3Total = 0;
+
+        private double allgarage = 0;
+
+
+        public ParkingGarage(double totalHours)
+        {
+            totalHours = totalHours;
+        }
+
+        public double calculateCharges()
+        {
+
+            if (totalHours <= 3)
+            {
+                allgarage = minimumFee;
+
+            }
+            else if (totalHours > 3 && totalHours < 19)
+            {
+                allgarage = minimumFee + ((totalHours - 3) * (additionalFeeG1 + additionalFeeG2 + additionalFeeG3);
+
+            }
+            else
+            {
+
+                allgarage = maxCharge;
+            }
+
+            return allgarage;
+        }
+
+    }
+
+
